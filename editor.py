8ed4eb1596f5a390cd3c51af94b585abce8b4eac
Your Name <you@example.com>
2013-08-30 01:12:55 +0200

    switch to pyglet and add main.py

--- editor.py
+++ editor.py
@@ -1,27 +1,46 @@
-__author__ = 'ankhmorporkian'
+__author__ = 'ankhmorporkian & sirdancealot'
 from lemon_logger import LemonLogger
-import pygame
 from element import *
+
+import pyglet
+from pyglet.gl import *
 
 logger = LemonLogger()
 
-class Editor(object):
+
+class EditorWindow(pyglet.window.Window):
     """
     Editor class, creates an editing window for the lemon language.
     """
-    def __init__(self, title='Lemon editor', width=640, height=480, font="monospace", size=18):
-        pygame.init()
-        pygame.display.set_caption(title)
-        self.setFont(font, size)
-        self.window = pygame.display.set_mode((width, height))
-        self.screen = pygame.display.get_surface()
-        self.root_element = Element()
+
+    def __init__(self, title='Lemon editor', font="monospace", size=18):
+        super(EditorWindow, self).__init__(caption=title,
+                                           resizable=True)
+        self.setupGl()
+        self.setFont()
+        self.root_element = Element(root = True)
+
+    def on_resize(self, width, height):
+        super(EditorWindow, self).on_resize(width, height)
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+    def setupGl(self):
+        glClearColor(1, 1, 1, 1)
+
+    def on_draw(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        glColor3f(.5, .5, .5)
+        glRectf(-.1, -.1, .1, .1)
+        glColor3f(.05, .05, .05)
+        glRectf(-.01, -.01, .01, .01)
 
     def setFont(self, font="monospace", size=18):
-        self.font = pygame.font.SysFont(font, size)
-        self.letter_width = self.font.render(" ",False,(0,0,0)).get_rect().width
-        self.letter_height = self.font.get_height()
-
-    def renderLoop(self):
-        self.root_element.render()
-
+#        self.font = pygame.font.SysFont(font, size)
+#        self.letter_width = self.font.render(" ",False,(0,0,0)).get_rect().width
+#        self.letter_height = self.font.get_height()
+	 pass
+	
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import pyglet
+
+import editor
+
+editor_window_1 = editor.EditorWindow()
+
+pyglet.app.run()
+
