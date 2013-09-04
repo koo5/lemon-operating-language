4e7d9f1a263c5f676940b0f94be7c2c74fd867f1
Your Name <you@example.com>
2013-09-04 22:21:45 +0200

    before focusing on the ast

--- aside_tree.py
+++ aside_tree.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
 
-#simulates a formated document + keeps track of indentation
-class test_document(object):
+
+
+"""
+
+a bit of a mess here but this could correspond to a subclass of a formatted document or work in a similar fashion
+
+"""
+
+class Document(object):
+    pass
+
+class test_document(Document):
 	def __init__(self):
 	    self.text = ""
 	    self.indentation = 0
 	def insert_text(self, pos, text, attributes):
 	    self.text += text
 	def __del__(self):
-	    print self.text.replace("\n","\\n\n")
+		#print self.text.replace("\n","\\n\n")
+		print self.text
 	def indent(self):
 	    self.indentation += 1
 	def dedent(self):
@@ -19,33 +30,30 @@ class test_document(object):
 
 
 
+
+
+
+
+"""
+
+things that make up a template
+
+"""
+
 def render_string(document, text, attributes):
 	document.insert_text(len(document.text),text, attributes)
-
 
 class newline(object):
 	def render(self, document):
 		document.append(document.indentation*"    " + "\n", {"node":self})#
 
-		
-newline().render(test_document(),{})
+class indent(object):
+	def render(self, document):
+		document.indent()
 
-class template(object):
-	def __init__(self, items):
-	    self.items = items
-	def render(self, document, node):
-	    for item in self.items:
-	        if isinstance(item,str):
-	            render_string(document, item, {"node":node})
-	        else:
-	            item.render(document)
-		newline().render({"node":node})
-
-
-template(["begin", " fun", newline(), " do stuff", newline(), " do stuff", newline(), "end"]).render(test_document(), "funs")
-
-
-			
+class dedent(object):
+	def render(self, document):
+		document.dedent()
 
 class placeholder(object):
 	def __init__(self, text):
@@ -55,9 +63,27 @@ class placeholder(object):
 	#def replace(self, replacement):
 	#	parent.children[self.name] = replacement...
 
-        
 
-template([[0,placeholder("thingy"),"456"]]).render(test_document(), 0, "banana")
+
+
+
+
+class template(object):
+	def __init__(self, items):
+	    self.items = items
+	def render(self, document, node):
+	    assert isinstance(document, Document)
+	    for item in self.items:
+	        if isinstance(item,str):
+	            render_string(document, item, {"node":node})
+	        else:
+	            item.render(document)
+
+
+
+			
+
+        
 
         
 
@@ -65,8 +91,8 @@ class node(object):
 	def __init__(self):
 		self.template_index = 0
 	template = property (lambda self: self.templates[self.template_index])
-	def render(self, document, indent):
-		self.template.render(document, indent,self)
+	def render(self, document):
+		self.template.render(document, self)
 	def prev_template(self):
 	    self.template_index  -= 1
 	    if self.template_index < 0:
@@ -76,48 +102,75 @@ class node(object):
 	    if self.template_index == len(self.templates):
 	        self.template_index = len(self.templates)-1
 
-	        
-test = node()
-test.templates = [template([[0,"aaa"]])]
-test.render(test_document(), 2)
 
-                        
-		
-class nodes(node):
+
+
+
+
+	        
+"""
+a statements node needs a little more than a template
+"""
+
+class statements_node(node):
 	def __init__(self, items):
 		node.__init__(self)
 		self.items = items
-	def render(self, document, indent):
+	def render(self, document):
 		for item in self.items:
 			item.render(document)
 			newline().render(document)
 
-nodes([placeholder("aaa"), placeholder("bbb"), placeholder("ccc")]).render(test_document(), 0)
-			
-class root_node(nodes):
-	def __init__(self,items):
-		nodes.__init__(self,items)
-		self.templates = [template([[0, "program:"],[1, placeholder("nodes")]])]
+
+
+
+
+class root_node(node):
+	def __init__(self,statements, author="banana", date_created="1.1.1.1111"):
+		node.__init__(self)
+		self.templates = [template(["program by ",member("author"), indent(), newline(), member("nodes"), dedent()])]
+		self.statements = statements
+		self.author = text(author)
+		self.date_created = text(date_created)
 
 	def render(self, document):
-		self.template.render(self, document)
-		nodes.render(self, document, 1)
+		self.template.render(document, self)
+		nodes.render(self, document)
 
 class while_node(node):
 	def __init__(self,condition,body):
 		self.condition = condition
 		self.body = body
-		self.templates = [template([[0,"while", placeholder("condition"), "do:"],[1,placeholder("code to execute")]]),template([[0,"repeat if", placeholder("condition"), "is true:"],[1,placeholder("code to execute")],[0,"go back up.."]])]
+		self.templates = [template(["while ", placeholder("condition"), " do:",newline(),indent(),placeholder("code to execute"),dedent()])]
+		#,template([[0,"repeat if", placeholder("condition"), "is true:"],[1,placeholder("code to execute")],[0,"go back up.."]])]
 
 class asignment_node(node):
 	def __init__(self, name, value):
 		node.__init__(self)
 		self.name=name
 		self.value = value
-		self.templates=[template([[0, placeholder("variable name"), " = ", placeholder("value")]]),
-									template([[0,"set ", placeholder("variable name"), " to ", placeholder("value")]]),
-									template([[0, "have ", placeholder("variable name"), " be ", placeholder("value")]])]
+		self.templates=[template([placeholder("variable name"), " = ", placeholder("value")]),
+									template(["set ", placeholder("variable name"), " to ", placeholder("value")]),
+									template(["have ", placeholder("variable name"), " be ", placeholder("value")])]
 									
+
+
+newline().render(test_document())
+
+template(["begin", " fun", newline(), "do stuff", newline(), "do stuff", newline(), "end"]).render(test_document(), "funs")
+
+template([placeholder("thingy"), " 456"]).render(test_document(), "banana")
+
+
+        
+test = node()
+test.templates = [template(["aaa"])]
+test.render(test_document())
+
+
+
+nodes([placeholder("aaa"), placeholder("bbb"), placeholder("ccc")]).render(test_document())
+
 
 root = root_node([asignment_node("a", 0)]) 
 #									while_node(is_less_than_node(variable_node("a"), constant_node(4)),
@@ -128,6 +181,6 @@ root = root_node([asignment_node("a", 0)])
 root.render(test_document())
 
 root.items[0].prev_template()
-root.render(test_document(), 0)
+root.render(test_document())
 root.items[0].next_template()
-root.render(test_document(), 0)
+root.render(test_document())
