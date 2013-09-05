94e79760c3713c4b28db506d0ba242247aac9acf
Your Name <you@example.com>
2013-09-05 11:15:35 +0200

    first aside_tree with incremental_test commit. indentation and styles are broken.

--- incremental_test.py
+++ incremental_test.py
@@ -1,230 +1,150 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-'''Demonstrates basic use of IncrementalTextLayout and Caret.
-
-A simple widget-like system is created in this example supporting keyboard and
-mouse focus.
-'''
-
-__docformat__ = 'restructuredtext'
-__version__ = '$Id: $'
 
 import pyglet
 
+import aside_tree as ast
 
-class Rectangle(object):
+class CodeArea(ast.Document):
 
-    '''Draws a rectangle into a batch.'''
+	def __init__(self, width, height, batch):
+		ast.Document.__init__(self)
 
-    def __init__(
-        self,
-        x1,
-        y1,
-        x2,
-        y2,
-        batch,
-        ):
+		self.batch = batch
+		
+		self.document = pyglet.text.document.FormattedDocument("ABC")
+		self.document.set_style(0, len(self.document.text),
+								dict(color=(255, 255, 255, 255)))
 
-        self.vertex_list = batch.add(4, pyglet.gl.GL_QUADS, None, ('v2i'
-                , [
-            x1,
-            y1,
-            x2,
-            y1,
-            x2,
-            y2,
-            x1,
-            y2,
-            ]), ('c4B', [200, 200, 220, 255] * 4))
+		self.layout = pyglet.text.layout.IncrementalTextLayout(
+					self.document, width, height, multiline=True, batch=batch)
+					
+		self.caret = pyglet.text.caret.Caret(self.layout)
 
+		self.layout.x = 0
+		self.layout.y = 0
 
-class DocumentElement(object):
+	def resize(self, width, height):
+		self.layout.width = width
+		self.layout.height = height
 
-    def __init__(
-        self,
-        text,
-        x,
-        y,
-        width,
-        height,
-        batch,
-        ):
-
-        self.document = pyglet.text.document.UnformattedDocument(text)
-        self.document.set_style(0, len(self.document.text),
-                                dict(color=(0, 0, 0, 255)))
-
-        self.layout = \
-            pyglet.text.layout.IncrementalTextLayout(self.document,
-                width, height, multiline=True, batch=batch)
-        self.caret = pyglet.text.caret.Caret(self.layout)
-
-        self.layout.x = x
-        self.layout.y = y
-
-        # Rectangular outline
-
-        self.pad = 2
-        self.rectangle = Rectangle(self.layout.x - self.pad,
-                                   self.layout.y - self.pad,
-                                   self.layout.x + width + self.pad,
-                                   self.layout.y + height + self.pad,
-                                   batch)
-
-    def hit_test(self, x, y):
-        return 0 < x - self.layout.x < self.layout.width and 0 < y \
-            - self.layout.y < self.layout.height
-
-    def resize(
-        self,
-        width,
-        height,
-        batch,
-        ):
-
-        self.layout.width = width
-        self.layout.height = height
-        self.rectangle = Rectangle(self.layout.x - self.pad,
-                                   self.layout.y - self.pad,
-                                   self.layout.x + width + self.pad,
-                                   self.layout.y + height + self.pad,
-                                   batch)
+	def append(self, text, attributes):
+		self.document.insert_text(len(self.document.text),text, attributes)
 
 
 class Window(pyglet.window.Window):
 
-    def __init__(self, *args, **kwargs):
-        super(Window, self).__init__(400, 140, caption='',
-                resizable=True)
+	def __init__(self, *args, **kwargs):
+		super(Window, self).__init__(640, 400, caption='lemon is at it again',
+				resizable=True)
 
-        self.batch = pyglet.graphics.Batch()
-        self.labels = []
-        self.widgets = [DocumentElement(
-            '',
-            0,
-            0,
-            self.width,
-            self.height / 2,
-            self.batch,
-            ), DocumentElement(
-            '',
-            0,
-            self.height / 2,
-            self.width,
-            self.height,
-            self.batch,
-            )]
-        self.text_cursor = self.get_system_mouse_cursor('text')
-
-        self.focus = None
-        self.set_focus(self.widgets[0])
-
-    def on_resize(self, width, height):
-        super(Window, self).on_resize(width, height)
-        for widget in self.widgets:
-            widget.resize(width, height, self.batch)
-
-    def on_draw(self):
-        pyglet.gl.glClearColor(1, 1, 1, 1)
-        self.clear()
-        self.batch.draw()
-
-    def on_mouse_motion(
-        self,
-        x,
-        y,
-        dx,
-        dy,
-        ):
-
-        for widget in self.widgets:
-            if widget.hit_test(x, y):
-                self.set_mouse_cursor(self.text_cursor)
-                break
-        else:
-            self.set_mouse_cursor(None)
-
-    def on_mouse_press(
-        self,
-        x,
-        y,
-        button,
-        modifiers,
-        ):
-
-        for widget in self.widgets:
-            if widget.hit_test(x, y):
-                self.set_focus(widget)
-                break
-        else:
-            self.set_focus(None)
-
-        if self.focus:
-            self.focus.caret.on_mouse_press(x, y, button, modifiers)
-
-    def on_mouse_drag(
-        self,
-        x,
-        y,
-        dx,
-        dy,
-        buttons,
-        modifiers,
-        ):
-
-        if self.focus:
-            self.focus.caret.on_mouse_drag(
-                x,
-                y,
-                dx,
-                dy,
-                buttons,
-                modifiers,
-                )
-
-    def on_text(self, text):
-        if self.focus:
-            self.focus.caret.on_text(text)
-
-    def on_text_motion(self, motion):
-        if self.focus:
-            self.focus.caret.on_text_motion(motion)
-
-    def on_text_motion_select(self, motion):
-        if self.focus:
-            self.focus.caret.on_text_motion_select(motion)
-
-    def on_key_press(self, symbol, modifiers):
-        if symbol == pyglet.window.key.TAB:
-            if modifiers & pyglet.window.key.MOD_SHIFT:
-                direction = -1
-            else:
-                direction = 1
-
-            if self.focus in self.widgets:
-                i = self.widgets.index(self.focus)
-            else:
-                i = 0
-                direction = 0
-
-            self.set_focus(self.widgets[(i + direction)
-                           % len(self.widgets)])
-        elif symbol == pyglet.window.key.ESCAPE:
-
-            pyglet.app.exit()
-
-    def set_focus(self, focus):
-        if self.focus:
-            self.focus.caret.visible = False
-            self.focus.caret.mark = self.focus.caret.position = 0
-
-        self.focus = focus
-        if self.focus:
-            self.focus.caret.visible = True
-            self.focus.caret.mark = 0
-            self.focus.caret.position = len(self.focus.document.text)
+		self.batch = pyglet.graphics.Batch()
+		self.code = CodeArea(self.width, self.height, self.batch)
+		
+		ast.root.render(self.code)
 
 
+	def on_resize(self, width, height):
+		super(Window, self).on_resize(width, height)
+		self.code.resize(width, height)
+
+	def on_draw(self):
+#		pyglet.gl.glClearColor(0, 0, 0, 1)
+		self.clear()
+		self.batch.draw()
+	"""
+	def on_mouse_motion(
+		self,
+		x,
+		y,
+		dx,
+		dy,
+		):
+
+		for widget in self.widgets:
+			if widget.hit_test(x, y):
+				self.set_mouse_cursor(self.text_cursor)
+				break
+		else:
+			self.set_mouse_cursor(None)
+
+	def on_mouse_press(
+		self,
+		x,
+		y,
+		button,
+		modifiers,
+		):
+
+		for widget in self.widgets:
+			if widget.hit_test(x, y):
+				self.set_focus(widget)
+				break
+		else:
+			self.set_focus(None)
+
+		if self.focus:
+			self.focus.caret.on_mouse_press(x, y, button, modifiers)
+
+	def on_mouse_drag(
+		self,
+		x,
+		y,
+		dx,
+		dy,
+		buttons,
+		modifiers,
+		):
+
+		if self.focus:
+			self.focus.caret.on_mouse_drag(
+				x,
+				y,
+				dx,
+				dy,
+				buttons,
+				modifiers,
+				)
+	"""
+	def on_text(self, text):
+		self.code.caret.on_text(text)
+
+	def on_text_motion(self, motion):
+		self.code.caret.on_text_motion(motion)
+
+	def on_text_motion_select(self, motion):
+		self.code.caret.on_text_motion_select(motion)
+	"""
+	def on_key_press(self, symbol, modifiers):
+		if symbol == pyglet.window.key.TAB:
+			if modifiers & pyglet.window.key.MOD_SHIFT:
+				direction = -1
+			else:
+				direction = 1
+
+			if self.focus in self.widgets:
+				i = self.widgets.index(self.focus)
+			else:
+				i = 0
+				direction = 0
+
+			self.set_focus(self.widgets[(i + direction)
+						   % len(self.widgets)])
+		elif symbol == pyglet.window.key.ESCAPE:
+
+			pyglet.app.exit()
+
+	def set_focus(self, focus):
+		if self.focus:
+			self.focus.caret.visible = False
+			self.focus.caret.mark = self.focus.caret.position = 0
+
+		self.focus = focus
+		if self.focus:
+			self.focus.caret.visible = True
+			self.focus.caret.mark = 0
+			self.focus.caret.position = len(self.focus.document.text)
+
+	"""
 window = Window(resizable=True)
 pyglet.app.run()
