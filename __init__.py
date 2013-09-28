40bc8ef26f6f5d83abe989bb8410fce72586d90b
Your Name <you@example.com>
2013-09-28 14:40:15 +0200

    a big cleanup

--- __init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python2
-# -*- coding: utf-8 -*-
-__author__ = 'ankhmorporkian'
--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2
-# -*- coding: utf-8 -*-
-
-import editor
-import pyglet
-
-editor_window_1 = editor.EditorWindow()
-
-pyglet.app.run()
